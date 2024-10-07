18fe0b57b2fd122d608598c06a72bd39c464a182
extinguishedsoul <vilordimarianna343@gmail.com>
2024-10-07 20:53:46 +0300

    Add files via upload

---
+++ lbrtrn2tsk1.pas
@@ -0,0 +1,11 @@
+﻿program tblcmnzhn;
+var chsl, i: Integer;
+begin
+write('Введите число: ');
+readLn(chsl);  
+writeLn('Таблица умножения для ', chsl);
+  for i:= 1 to 10 do
+ begin
+writeLn(chsl, ' x ', i, ' = ', chsl * i);
+ end;
+end.
