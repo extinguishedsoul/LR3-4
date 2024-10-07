8f86b63cadebcd6dc01422821331442b8d2e6c89
extinguishedsoul <vilordimarianna343@gmail.com>
2024-10-07 20:43:08 +0300

    Add files via upload

---
+++ lbrtrn2tsk2.pas
@@ -0,0 +1,13 @@
+﻿program lbrtrn2tsk2;
+var gd:Integer; clrzh,rzh:Real;
+begin
+gd:= 1998;
+rzh:= 20;
+clrzh:= 30;  
+while rzh <= clrzh do
+begin
+rzh:= rzh * 1.04;  
+gd:=gd + 2;            
+end;
+WriteLn('Урожайность превысит ', clrzh, ' центнеров с га в ', gd, ' году.');
+end.
