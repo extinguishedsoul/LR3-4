9086760ec3c6f5f99a5f7e8d3bf416c9b4493631
extinguishedsoul <vilordimarianna343@gmail.com>
2024-10-07 20:33:17 +0300

    Add files via upload

--- lbrtrn2tsk2.py
+++ lbrtrn2tsk2.py
@@ -1,8 +1,7 @@
-rzh = 1998
-rvnrzhnst = 0.04
-cl = 30 
-gd = 1998
-while rzh < cl:
-    gd += 2
-    rzh += rzh * rvnrzhnst
-print("Урожайность превысит 30 центнеров с га в", gd, "году")
+gd = 1998  
+rzh = 20
+clrzh = 30  
+while rzh <= clrzh:
+    rzh *= 1.04  
+    gd += 2 
+print(f"Урожайность превысит {clrzh} центнеров с га в {gd} году.")
